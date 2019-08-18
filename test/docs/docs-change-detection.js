aa5c8d8b946dfc21f989a7ce497abf424a992f7e
Adam Plocieniak <adam.plocieniak@allegro.pl>
2019-08-18 06:03:42 +0200

    notify github logic added

--- test/docs/docs-change-detection.js
+++ test/docs/docs-change-detection.js
@@ -2,20 +2,28 @@ const should = require('should');
 const fs = require('fs');
 const md5 = require('md5');
 const path = require('path');
+const request = require('request');
 const expect = require('expect.js');
 const generate = require('../../src/generate-api-description.js');
 
 const API_DESC_FILE_PATH = path.join(__dirname, '../../dist/api-examples.json');
 
-describe('Docs change detection', function() {
+describe.only('Docs change detection', function() {
     this.timeout(16000);
     describe('', () => {
+        afterEach(function(done) {
+            if(this.currentTest.state === 'failed'){
+               notifyGithub(done);
+            } else {
+                done();
+            }
+        })
         it('contains equal content', function (done) {
             const currentDocsDescription = normalizeString(fs.readFileSync(API_DESC_FILE_PATH).toString());
             generate((err, retrievedDocsDescription) => {
                 expect(err).to.be(null);
                 retrievedDocsDescription = normalizeString(retrievedDocsDescription);
-                expect(md5(retrievedDocsDescription)).to.be(md5(currentDocsDescription));
+                expect(md5(retrievedDocsDescription)).to.be('1'+md5(currentDocsDescription));
                 done();
             });
         });
@@ -24,3 +32,23 @@ describe('Docs change detection', function() {
         }
     });
 });
+
+function notifyGithub(done) {
+    const authorization = `Basic ${Buffer.from(`adasq:${process.env.TOKEN}`).toString('base64')}`
+    request.post({
+        url: 'https://api.github.com/repos/adasq/dropbox-v2-api/dispatches',
+        body: { event_type : 'html.preview' },
+        json: true,
+        headers: {
+            'User-Agent': 'curl/7.54.0',
+            Authorization: authorization,
+            'Content-Type': 'application/json',
+            Accept: 'application/vnd.github.everest-preview+json'
+        }
+    }, (err, resp, body) => {
+        console.log(err);
+        console.log(resp.statusCode);
+        console.log(body);
+        done()
+    })
+}
