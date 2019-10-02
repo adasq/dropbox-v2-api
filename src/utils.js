68a33ebce120bb7901f8817d403165452bbd52bc
Adam Plocieniak <adam.plocieniak@allegro.pl>
2019-10-02 21:23:42 +0200

    code cleanup #1

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 const tester = require('stream-tester');
 
 /**
-* Cretes read stream, which generates length-sized stream of characters (sign)
+* Creates read stream, which generates length-sized stream of characters (sign)
 * Total length of stream is (length * sign.length)
 */
 function createMockedReadStream(sign, length){
