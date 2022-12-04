75ec1b1f843c325760ce821d60cc81cbafee655a
Simone Gosetto <46279708+simonegosetto@users.noreply.github.com>
2022-12-04 19:57:39 +0100

    feat(api.json): compressed api.json for save space and for better compilation with webpack (#1215)
    * feat(api.json): compressed api.json for save space and for better compilation with webpack
    
    * feat(api.json): compressed api.json for save space and for better compilation with webpack

--- generate.js
+++ generate.js
@@ -1,1 +1,4 @@
-require('./src/generate-api-description.js')();
+// require('./src/generate-api-description.js')();
+const {decompress} = require("compress-json");
+const apiJSON = require('./src/api.json');
+console.log(decompress(apiJSON));
