4f1cd03f006cc6331849a4aaf3a374a7070c348b
adasq <adasq@users.noreply.github.com>
2022-12-05 06:39:47 +0100

    Revert "feat(api.json): compressed api.json for save space and for better compilation with webpack (#1215)"
    This reverts commit 75ec1b1f843c325760ce821d60cc81cbafee655a.

--- generate.js
+++ generate.js
@@ -1,4 +1,1 @@
-// require('./src/generate-api-description.js')();
-const {decompress} = require("compress-json");
-const apiJSON = require('./src/api.json');
-console.log(decompress(apiJSON));
+require('./src/generate-api-description.js')();
